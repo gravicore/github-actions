fe80473b4691848d73caffc52c693e7a0a740817
Darlan Silveira <darlan.silveira@gravicore.io>
2025-07-22 15:05:00 -0300

    GDEV-413: adding javascript support on sonar

--- .github/actions/sonar-scan/merge.sh
+++ .github/actions/sonar-scan/merge.sh
@@ -18,21 +18,37 @@ if echo "${SONAR}" | jq -e 'has("python")' > /dev/null; then
   SONAR=$(echo "${SONAR}" | jq -c ".python.[\"ignore-errors\"] = (if .python.[\"ignore-errors\"] == \"\" or .python.[\"ignore-errors\"] == null then \"${DEFAULT_PYTHON_IGNORE_ERRORS}\" else .python.[\"ignore-errors\"] end)")
 fi
 
-SOURCES=$(echo "${SONAR}" | jq -rc '[
-  (has("java") as $j | has("python") as $p |
-    if $j and $p then "java,python"
-    elif $j then "java"
-    elif $p then "python"
-    else "" end)
-  ] | .[0]')
+if echo "${SONAR}" | jq -e 'has("javascript")' > /dev/null; then
+  SONAR=$(echo "${SONAR}" | jq -c ".javascript.path = (if .javascript.path == \"\" or .javascript.path == null then \"${DEFAULT_JAVASCRIPT_PATH}\" else .javascript.path end)")
+  SONAR=$(echo "${SONAR}" | jq -c ".javascript.version = (if .javascript.version == \"\" or .javascript.version == null then \"${DEFAULT_JAVASCRIPT_VERSION}\" else .javascript.version end)")
+  SONAR=$(echo "${SONAR}" | jq -c ".javascript.command = (if .javascript.command == \"\" or .javascript.command == null then \"${DEFAULT_JAVASCRIPT_COMMAND}\" else .javascript.command end)")
+  SONAR=$(echo "${SONAR}" | jq -c ".javascript.pattern = (if .javascript.pattern == \"\" or .javascript.pattern == null then \"${DEFAULT_JAVASCRIPT_PATTERN}\" else .javascript.pattern end)")
+  SONAR=$(echo "${SONAR}" | jq -c ".javascript.[\"ignore-errors\"] = (if .javascript.[\"ignore-errors\"] == \"\" or .javascript.[\"ignore-errors\"] == null then \"${DEFAULT_JAVASCRIPT_IGNORE_ERRORS}\" else .javascript.[\"ignore-errors\"] end)")
+fi
 
-LANGUAGES=$(echo "${SONAR}" | jq -rc '[
-  (has("java") as $j | has("python") as $p |
-    if $j and $p then "java,py"
-    elif $j then "java"
-    elif $p then "py"
-    else "" end)
-  ] | .[0]')
+SOURCES=$(echo "${SONAR}" | jq -rc "[
+  (has(\"java\") as \$jv | has(\"python\") as \$py | has(\"javascript\") as \$js |
+    if \$jv and \$py and \$js then \"\(.java.path),\(.python.path),\(.javascript.path)\"
+    elif \$jv and \$py then \"\(.java.path),\(.python.path)\"
+    elif \$jv and \$js then \"\(.java.path),\(.javascript.path)\"
+    elif \$py and \$js then \"\(.python.path),\(.javascript.path)\"
+    elif \$jv then \"\(.java.path)\"
+    elif \$py then \"\(.python.path)\"
+    elif \$js then \"\(.javascript.path)\"
+    else \"\" end)
+  ] | .[0]")
+
+LANGUAGES=$(echo "${SONAR}" | jq -rc "[
+  (has(\"java\") as \$jv | has(\"python\") as \$py | has(\"javascript\") as \$js |
+    if \$jv and \$py and \$js then \"java,py,js\"
+    elif \$jv and \$py then \"java,py\"
+    elif \$jv and \$js then \"java,js\"
+    elif \$py and \$js then \"py,js\"
+    elif \$jv then \"java\"
+    elif \$py then \"py\"
+    elif \$js then \"js\"
+    else \"\" end)
+  ] | .[0]")
 
 SONAR=$(echo "${SONAR}" | jq -c ".sources = \"${SOURCES}\"")
 SONAR=$(echo "${SONAR}" | jq -c ".languages = \"${LANGUAGES}\"")
